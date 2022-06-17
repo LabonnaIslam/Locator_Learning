2822d9e794a89e0c61b2426da49b3b2d5f5d9149
LabonnaIslam <labonnaislamtonima@gamil.com>
2022-06-18 00:26:05 +0600

    locator learning

--- LocatorlearningClass/src/main/java/testCases/TC001_LocatorLearning.java
+++ LocatorlearningClass/src/main/java/testCases/TC001_LocatorLearning.java
@@ -38,7 +38,40 @@ public class TC001_LocatorLearning extends DriverSetup {
 		//inout[@attribute='value'] 
 		
 		
-		//driver.findElement(By.xpath(""))
+		//driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("toni");
+		//Thread.sleep(2000);
+		
+	//	driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("toni@gnail.com");
+		
+
+		driver.findElement(By.xpath("//input[@type='text'][3]")).sendKeys("76213");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//input[@type='text'][1]")).sendKeys("toni");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("toni@gnail.com");
+		
+		
+		
+		driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]" )).click();
+		Thread.sleep(2000);
+		
+		
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("Tanvir");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+		Thread.sleep(2000);
+		driver.findElement(By.id("chkboxOne")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+		Thread.sleep(2000);
+
+
+
 	
 	}
 
