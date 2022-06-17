9737d4b22524b9e8e511889bb179412661f931ec
LabonnaIslam <labonnaislamtonima@gamil.com>
2022-06-18 00:02:11 +0600

    locator learning

--- LocatorlearningClass/src/main/java/testCases/TC001_LocatorLearning.java
+++ LocatorlearningClass/src/main/java/testCases/TC001_LocatorLearning.java
@@ -1,5 +1,6 @@
 package testCases;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import base.DriverSetup;
@@ -13,6 +14,32 @@ public class TC001_LocatorLearning extends DriverSetup {
 		driver.get(baseurl);
 		driver.manage().window().maximize();
 		Thread.sleep(2000);
+		
+		
+		driver.findElement(By.id("inputUsername")).sendKeys("Labonna");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.name("inputPassword")).sendKeys("bioooo");
+		Thread.sleep(2000);
+		
+		
+		driver.findElement(By.className("signInBtn")).click();
+		Thread.sleep(2000);
+		
+		
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+		//Formula:	tagname.value(if class is given)
+		//			tagname#value(if id is given)
+		
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(2000);
+		
+		//driver.findElement(By.xpath("//input[@placeholder='name']")).sendKeys("toni");
+		//inout[@attribute='value'] 
+		
+		
+		//driver.findElement(By.xpath(""))
+	
 	}
 
 }
